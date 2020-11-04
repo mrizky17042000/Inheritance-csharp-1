1a4d1783be6c23538f7c23819ef78193e5b37b90
mrizky17042000 <mriski889@gmail.com>
2020-11-04 10:28:08 +0700

    Latihan 1 Pemrograman WEB 2

---
+++ Kampus.cs
@@ -0,0 +1,113 @@
+using System;
+
+namespace Latihan1
+{
+    public class Kampus
+    {
+        String dosen;
+        String matkul;
+        public void setDosen(String dosen)
+        {
+            this.dosen = dosen;
+        }
+        public String getDosen()
+        {
+            return this.dosen;
+        }
+        public void setMatkul(String matkul)
+        {
+            this.matkul = matkul;
+        }
+        public String getMatkul()
+        {
+            return this.matkul;
+        }
+
+        public class Mahasiswa : Kampus
+        {
+            int id;
+            String nama;
+            String alamat;
+            String kelas;
+            // ==============================CONSTRUCT NULL============================
+            // public Mahasiswa()
+            // {
+
+            // }
+            public void setId(int id)
+            {
+                this.id = id;
+            }
+            public int getId()
+            {
+                return this.id;
+            }
+            public void setNama(String nama)
+            {
+                this.nama = nama;
+            }
+            public string getNama()
+            {
+                return this.nama;
+            }
+
+            public void setAlamat(String alamat)
+            {
+                this.alamat = alamat;
+            }
+            public string getAlamat()
+            {
+                return this.alamat;
+            }
+
+            public void setKelas(String kelas)
+            {
+                this.kelas = kelas;
+            }
+            public String getKelas()
+            {
+                return this.kelas;
+            }
+            public class Ukm : Mahasiswa
+            {
+                string ukm;
+                // =========================== CONSTRUCT INISIALISASI =======================
+                public Ukm(int id, String nama, String alamat, String kelas, String dosen, String matkul, string ukm)
+                {
+                    Console.WriteLine("=============== Profile ================");
+                    this.id = id;
+                    this.nama = nama;
+                    this.alamat = alamat;
+                    this.kelas = kelas;
+                    this.dosen = dosen;
+                    this.matkul = matkul;
+                    this.ukm = ukm;
+                }
+                public string getUkm()
+                {
+                    return this.ukm;
+                }
+
+                public void setUkm(string ukm)
+                {
+                    this.ukm = ukm;
+                }
+                public void Organisasi()
+                {
+                    Console.WriteLine("Organisasi Adalah " + getUkm());
+                }
+                public void printInfo()
+                {
+                    Console.WriteLine("NPM saya adalah = " + getId());
+                    Console.WriteLine("Nama saya adalah = " + getNama());
+                    Console.WriteLine("Alamat saya adalah = " + getAlamat());
+                    Console.WriteLine("Kelas saya adalah = " + getKelas());
+                    Console.WriteLine("Nama Dosen saya adalah = " + getDosen());
+                    Console.WriteLine("Matkul saya adalah = " + getMatkul());
+                    Console.WriteLine("Ukm saya adalah = " + getUkm());
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Latihan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // =============================== INHERITANCE ===================================
+
+            // ========================= SET IN MAIN WITH CONSTRUCT======================
+            Kampus.Mahasiswa.Ukm kmps = new Kampus.Mahasiswa.Ukm(1194021, "M. RIZKY", "Sarijadi", "D4 TI - 2A", "Moh. Nurkamal Fauzan", "Pemrograman WEB 2", "Himatif");
+            kmps.printInfo();
+
+            // ========================= SET IN MAIN WITH CONSTRUCT AND VAR======================
+            // var kmps = new Kampus.Mahasiswa(20, "M. RIZKY", "Sarijadi", "D4 TI", "Moh Nurkamal Fauzan", "Pemrograman WEB 2");
+            // kmps.printInfo();
+        }
+    }
+}
